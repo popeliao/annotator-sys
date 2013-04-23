274008b0a5539757efe3b6fca79c3db80187f8b3
popeliao <popeliao@gmail.com>
2013-04-23 15:36:48 -0400

    mapping id for docs and use it

--- post-process/network/create_from_result.py
+++ post-process/network/create_from_result.py
@@ -10,59 +10,64 @@ April 2013
 """
 import sys
 import re
+from map_id import Map_ID_Loader
 
 class CityGraph(object):
     def __init__(self,files,silent = False):
         self.f0 = files[0]
         self.f1 = files[1]
         self.silent = silent
+        f = open("data_id_post.csv","r")
+        self.id_loader = Map_ID_Loader(f, True)
+        self.id_dict = self.id_loader.getDict()
+        f.close()
+
 
     def init(self):
     #build city count list on a doc base
         self.docNum = 0
-        s = self.extractDoc()
-        self.doc_dict = []
+        s, fid = self.extractDoc()
+        self.doc_dict = {}
         while s != "":                        
-            self.doc_dict.append({})
-            self.update_dict(s)
-            s = self.extractDoc()    
+            self.doc_dict[fid] = {}
+            self.update_dict(s, fid)
+            s, fid = self.extractDoc()   
 
-    def update_dict(self, docstr):
+    def update_dict(self, docstr, fid):
     #build the city count list for each doc
         p = re.compile(r'<NE:CITY.*?>.*?</NE:CITY>')
-        list = p.findall(docstr)
-        n = self.docNum - 1
+        list = p.findall(docstr)        
         for items in list:
             name = items[items.find('>')+1:items.find('<',items.find('<')+1)]
-            if self.doc_dict[n].has_key(name):
-                self.doc_dict[n][name] += 1
+            if self.doc_dict[fid].has_key(name):
+                self.doc_dict[fid][name] += 1
             else:
-                self.doc_dict[n][name] = 1
+                self.doc_dict[fid][name] = 1
 
 
     def out_city_count_list(self):
     #write the city count list to output file
-        self.f1.write("%20s %5s %5s\n"  %("CITY NAME", "DOC", "COUNT")) 
-        i = 0
-        for each_doc in self.doc_dict:
-            i += 1
-            if each_doc:
-                for each_city in each_doc:
-                    self.f1.write("%20s %5.0f %5.0f\n" %(each_city, i, each_doc[each_city]))              
+        self.f1.write("%20s %20s %5s\n"  %("CITY NAME", "DOC", "COUNT"))         
+        for doc_id in self.doc_dict: 
+            if self.doc_dict[doc_id]:
+                for each_city in self.doc_dict[doc_id]:
+                    self.f1.write("%20s %20s %5.0f\n" %(each_city, doc_id, self.doc_dict[doc_id][each_city]))              
 
     def extractDoc(self):
     #extract doc string from input file docly
         c = self.f0.readline()    
         s = ""
+        fid = ""
         while c and not "<doc>" in c:
             c = self.f0.readline()        
         if c:        
             c = self.f0.readline()
+            fid = self.id_dict[c.split()[0][:-1]]    # all http url is directly followed <doc> and has a extra ':' at the end
             while not "</doc>" in c:
                 s += c
                 c = self.f0.readline()        
             self.docNum += 1
-        return s
+        return s, fid
             
 
 def main(argv):
